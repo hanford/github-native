3b022c15e41ebc8d307439505c1fa48d18efdf09
Jack Hanford <jackhanford@gmail.com>
2014-08-29 13:23:17 -0700

    unifying viewS

--- www/js/service.js
+++ www/js/service.js
@@ -5,36 +5,54 @@ angular.module('service', [])
 	return {
 		getPerson: function(uname) {
 			var promise = $http.get(baseurl + 'users/' + uname).then(function(response) {
+				if (response.status == 403) {
+					alert('Rate Limit')
+				}
 				return response.data;
 			})
 			return promise;
 		},
 		getProjects: function(uname) {
 			var promise = $http.get(baseurl + 'search/repositories?q=' + uname).then(function(response) {
+				if (response.status == 403) {
+					alert('Rate Limit')
+				}
 				return response.data;
 			})
 			return promise;
 		},
 		getEvents: function(login) {
 			var promise = $http.get(baseurl + 'users/' + login + '/events').then(function(response) {
+				if (response.status == 403) {
+					alert('Rate Limit')
+				}
 				return response.data;
 			})
 			return promise;
 		},
 		userRepo: function(username) {
 			var promise = $http.get(baseurl + 'users/' + username + '/repos').then(function(response) {
+				if (response.status == 403) {
+					alert('Rate Limit')
+				}
 				return response.data;
 			})
 			return promise;
 		},
 		getFollowers: function(username) {
 			var promise = $http.get(baseurl + 'users/' + username + '/followers').then(function(response) {
+				if (response.status == 403) {
+					alert('Rate Limit')
+				}
 				return response.data;
 			})
 			return promise;
 		},
 		getFollowing: function(username) {
 			var promise = $http.get(baseurl + 'users/' + username + '/following').then(function(response) {
+				if (response.status == 403) {
+					alert('Rate Limit')
+				}
 				return response.data;
 			})
 			return promise;
@@ -42,6 +60,9 @@ angular.module('service', [])
 		getCommits: function(fullname) {
 			debugger
 			var promise = $http.get(baseurl + 'repos/' + fullname + '/commits').then(function(response) {
+				if (response.status == 403) {
+					alert('Rate Limit')
+				}
 				return response.data
 			})
 			return promise
