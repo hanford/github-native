7f1f9168e985960a7ca58dea8be00d358a1b2357
Jack Hanford <jackhanford@gmail.com>
2014-08-29 11:56:48 -0700

    New build, lots of errors fixed..

---
+++ platforms/ios/www/js/service.js
@@ -0,0 +1,50 @@
+angular.module('service', [])
+
+.factory('githubservice', function($http, $rootScope) {
+	var baseurl = 'https://api.github.com/'
+	return {
+		getPerson: function(uname) {
+			var promise = $http.get(baseurl + 'users/' + uname).then(function(response) {
+				return response.data;
+			})
+			return promise;
+		},
+		getProjects: function(uname) {
+			var promise = $http.get(baseurl + 'search/repositories?q=' + uname).then(function(response) {
+				return response.data;
+			})
+			return promise;
+		},
+		getEvents: function(login) {
+			var promise = $http.get(baseurl + 'users/' + login + '/events').then(function(response) {
+				return response.data;
+			})
+			return promise;
+		},
+		userRepo: function(username) {
+			var promise = $http.get(baseurl + 'users/' + username + '/repos').then(function(response) {
+				return response.data;
+			})
+			return promise;
+		},
+		getFollowers: function(username) {
+			var promise = $http.get(baseurl + 'users/' + username + '/followers').then(function(response) {
+				return response.data;
+			})
+			return promise;
+		},
+		getFollowing: function(username) {
+			var promise = $http.get(baseurl + 'users/' + username + '/following').then(function(response) {
+				return response.data;
+			})
+			return promise;
+		},
+		getCommits: function(fullname) {
+			debugger
+			var promise = $http.get(baseurl + 'repos/' + fullname + '/commits').then(function(response) {
+				return response.data
+			})
+			return promise
+		}
+	}
+});
--- www/js/controller.js
+++ www/js/controller.js
@@ -138,6 +138,7 @@ angular.module('controller', [])
 		githubservice.getFollowers($rootScope.uname).then(function(response){
 			$ionicLoading.hide();
 			$state.go('followers')
+			debugger
 			$rootScope.followers = response;
 		})
 	}
@@ -190,7 +191,7 @@ angular.module('controller', [])
 
 	$scope.recentActivity = function (repo) {
 		$scope.repo = repo;
-		githubservice.getCommits(repo.fullname).then(function(response){
+		githubservice.getCommits(repo).then(function(response){
 			$rootScope.commits = response;
 			$state.go('commits')
 		})
@@ -303,52 +304,3 @@ angular.module('controller', [])
 	}
 
 })
-
-
-.factory('githubservice', function($http, $rootScope) {
-	var baseurl = 'https://api.github.com/'
-	return {
-		getPerson: function(uname) {
-			var promise = $http.get(baseurl + 'users/' + uname).then(function(response) {
-				return response.data;
-			})
-			return promise;
-		},
-		getProjects: function(uname) {
-			var promise = $http.get(baseurl + 'search/repositories?q=' + uname).then(function(response) {
-				return response.data;
-			})
-			return promise;
-		},
-		getEvents: function(login) {
-			var promise = $http.get(baseurl + 'users/' + login + '/events').then(function(response) {
-				return response.data;
-			})
-			return promise;
-		},
-		userRepo: function(username) {
-			var promise = $http.get(baseurl + 'users/' + username + '/repos').then(function(response) {
-				return response.data;
-			})
-			return promise;
-		},
-		getFollowers: function(username) {
-			var promise = $http.get(baseurl + 'users/' + username + '/followers').then(function(response) {
-				return response.data;
-			})
-			return promise;
-		},
-		getFollowing: function(username) {
-			var promise = $http.get(baseurl + 'users/' + username + '/following').then(function(response) {
-				return response.data;
-			})
-			return promise;
-		},
-		getCommits: function(fullname) {
-			var promise = $http.get(baseurl + 'repos/' + fullname + '/commits').then(function(response) {
-				return response.data
-			})
-			return promise
-		}
-	}
-});
