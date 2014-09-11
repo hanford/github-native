d18168b2a16861c75a0621c63b745deef57b517e
Jack Hanford <jackhanford@gmail.com>
2014-09-11 12:49:11 -0700

    By network conf, hello pretty code

--- www/js/app.js
+++ www/js/app.js
@@ -1,38 +1,22 @@
-angular.module('MobGit', ['ionic', 'state', 'controller', 'factory', 'hljs', 'ngCordova.plugins.network'])
+angular.module('MobGit', ['ionic', 'state', 'controller', 'factory', 'hljs'])
 
-.run(function($ionicPlatform, $rootScope, $state, $timeout, $cordovaNetwork, $ionicPopup) {
-	$ionicPlatform.ready(function() {
+.run(function($ionicPlatform, $rootScope, $state, $timeout, $ionicPopup) {
+  $ionicPlatform.ready(function() {
 
-		console.log($cordovaNetwork.isOffline())
+    OAuth.initialize('DhJ5nGr1cd7KBlGv47FUpYq5goo')
 
-		// if ($cordovaNetwork.isOffline() == true) {
-		// 	showAlert = function() {
-		// 		var alertPopup = $ionicPopup.alert({
-		// 			title: 'Network Required',
-		// 			template: 'To use this Application you must first make sure you have access to the internet!'
-		// 		});
-		// 		alertPopup.then(function(res) {
+    $timeout(function() {
+      if (!$rootScope.access_token) {
+        $rootScope.showBack = false
+        $state.go('intro')
+      } else {
+        $rootScope.showBack = true
+        $state.go('search')
+      }
+    }, 500)
 
-		// 		});
-		// 	}
-		// 	showAlert()
-		// 	alert('no internet!')
-		// }
-
-			OAuth.initialize('DhJ5nGr1cd7KBlGv47FUpYq5goo')
-
-			$timeout(function(){
-				if(!$rootScope.access_token) {
-					$rootScope.showBack = false
-					$state.go('intro')
-				} else {
-					$rootScope.showBack = true
-					$state.go('search')
-				}
-			}, 500)
-
-			if(window.StatusBar) {
-				StatusBar.styleDefault();
-			}
-	})
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  })
 })
--- www/js/factory.js
+++ www/js/factory.js
@@ -1,92 +1,92 @@
 angular.module('factory', ['ionic'])
 
 .factory('githubservice', function($http, $rootScope, $ionicPopup) {
-	var baseurl = 'https://api.github.com/';
+  var baseurl = 'https://api.github.com/';
 
-	var $ajax = {
-		get: function(route) {
-			var query = '';
-			var args = Array.prototype.slice.call(arguments);
+  var $ajax = {
+    get: function(route) {
+      var query = '';
+      var args = Array.prototype.slice.call(arguments);
 
-			if (args.length > 1) {
-				query = '&' + args[1];
-			}
+      if (args.length > 1) {
+        query = '&' + args[1];
+      }
 
 
-			if (window.OAuth) {
-				return $http.get(route + '?access_token=' + $rootScope.access_token + query).then(function(response) {
-					return response.data;
-				}).catch(function(err) {
-					console.log(err)
-					showAlert = function() {
-						var alertPopup = $ionicPopup.alert({
-							title: 'Hmmm..',
-							template: 'It Looks like something went wrong'
-						});
-						alertPopup.then(function(res) {
+      if (window.OAuth) {
+        return $http.get(route + '?access_token=' + $rootScope.access_token + query).then(function(response) {
+          return response.data;
+        }).catch(function(err) {
+          console.log(err)
+          showAlert = function() {
+            var alertPopup = $ionicPopup.alert({
+              title: 'Hmmm..',
+              template: 'It Looks like something went wrong'
+            });
+            alertPopup.then(function(res) {
 
-						});
-					}
-					showAlert()
-				})
-			} else if ($rootScope.access_token == undefined) {
-				return $http.get(route).then(function(response) {
-					return response.data;
-				}).catch(function(error) {
-					alert(error)
-				})
-			}
-		}
-	};
+            });
+          }
+          showAlert()
+        })
+      } else if ($rootScope.access_token == undefined) {
+        return $http.get(route).then(function(response) {
+          return response.data;
+        }).catch(function(error) {
+          alert(error)
+        })
+      }
+    }
+  };
 
-	return {
-		getPerson: function(uname) {
-			var promise = $ajax.get(baseurl + 'users/' + uname)
-			return promise;
-		},
-		getProjects: function(uname) {
-			var promise = $ajax.get(baseurl + 'search/repositories', 'q=' + uname)
-			return promise;
-		},
-		getEvents: function(login) {
-			var promise = $ajax.get(baseurl + 'users/' + login + '/events');
-			return promise;
-		},
-		userRepo: function(username) {
-			var promise = $ajax.get(baseurl + 'users/' + username + '/repos')
-			return promise;
-		},
-		getFollowers: function(username) {
-			var promise = $ajax.get(baseurl + 'users/' + username + '/followers')
-			return promise;
-		},
-		getFollowing: function(username) {
-			var promise = $ajax.get(baseurl + 'users/' + username + '/following')
-			return promise;
-		},
-		getCommits: function(fullname) {
-			var promise = $ajax.get(baseurl + 'repos/' + fullname + '/commits')
-			return promise
-		},
-		getTree: function(fullname) {
-			var promise = $ajax.get(baseurl + 'repos/' + fullname + '/contents')
-			return promise
-		},
-		getStats: function(fullname) {
-			var promise = $ajax.get(baseurl + 'repos/' + fullname + '/stats/contributors')
-			return promise
-		},
-		getCode: function(fullname, path) {
-			var promise = $ajax.get(baseurl + 'repos/' + fullname + '/contents/' + path)
-			return promise
-		},
-		getDir: function(url) {
-			var promise = $ajax.get(url)
-			return promise
-		},
-		getRate: function() {
-			var promise = $ajax.get(baseurl + 'rate_limit')
-			return promise
-		}
-	}
+  return {
+    getPerson: function(uname) {
+      var promise = $ajax.get(baseurl + 'users/' + uname)
+      return promise;
+    },
+    getProjects: function(uname) {
+      var promise = $ajax.get(baseurl + 'search/repositories', 'q=' + uname)
+      return promise;
+    },
+    getEvents: function(login) {
+      var promise = $ajax.get(baseurl + 'users/' + login + '/events');
+      return promise;
+    },
+    userRepo: function(username) {
+      var promise = $ajax.get(baseurl + 'users/' + username + '/repos')
+      return promise;
+    },
+    getFollowers: function(username) {
+      var promise = $ajax.get(baseurl + 'users/' + username + '/followers')
+      return promise;
+    },
+    getFollowing: function(username) {
+      var promise = $ajax.get(baseurl + 'users/' + username + '/following')
+      return promise;
+    },
+    getCommits: function(fullname) {
+      var promise = $ajax.get(baseurl + 'repos/' + fullname + '/commits')
+      return promise
+    },
+    getTree: function(fullname) {
+      var promise = $ajax.get(baseurl + 'repos/' + fullname + '/contents')
+      return promise
+    },
+    getStats: function(fullname) {
+      var promise = $ajax.get(baseurl + 'repos/' + fullname + '/stats/contributors')
+      return promise
+    },
+    getCode: function(fullname, path) {
+      var promise = $ajax.get(baseurl + 'repos/' + fullname + '/contents/' + path)
+      return promise
+    },
+    getDir: function(url) {
+      var promise = $ajax.get(url)
+      return promise
+    },
+    getRate: function() {
+      var promise = $ajax.get(baseurl + 'rate_limit')
+      return promise
+    }
+  }
 });
