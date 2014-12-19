e75aed0fa425fed4f59802b5d9223acebfab3d7c
jack hanford <jackhanford@gmail.com>
2014-12-18 23:56:05 -0600

    restored followers

--- www/js/follow/follow.js
+++ www/js/follow/follow.js
@@ -1,14 +1,12 @@
 angular.module('follow', [])
 
-.controller('followerCtrl', function($scope, $http, $rootScope, $state, $ionicLoading, $ionicNavBarDelegate) {
+.controller('followerCtrl', function($scope, $http, $rootScope, $state, $ionicLoading, $ionicNavBarDelegate, $ionicHistory) {
   $scope.followers = $rootScope.followers;
   
   $scope.toFollower = function(fName) {
     $rootScope.uname = fName;
 
-    var url = 'https://api.github.com/users/' + fName;
-
-    console.log(url)
+    var url = 'https://api.github.com/users/' + fName + '?access_token=' + $rootScope.access_token;;
 
     $ionicLoading.show({
       template: '<i class="ion-loading-c"></i>'
@@ -16,7 +14,8 @@ angular.module('follow', [])
 
     $http.get(url).success(function(data, headers, status, config) {
       $rootScope.ginfo = data;
-      $ionicLoading.hide()
+      $ionicLoading.hide();
+      $ionicHistory.clearCache();
       $state.go('profile')
     }).error(function(data, headers, status, config) {
       console.log(data, headers, status)
@@ -24,7 +23,7 @@ angular.module('follow', [])
   }
 })
 
-.controller('followingCtrl', function($scope, $http, $rootScope, $state, $ionicLoading, $ionicNavBarDelegate) {
+.controller('followingCtrl', function($scope, $http, $rootScope, $state, $ionicLoading, $ionicNavBarDelegate, $ionicHistory) {
   $scope.followings = $rootScope.following;
   
   $scope.toFollower = function(fName) {
@@ -38,7 +37,8 @@ angular.module('follow', [])
 
     $http.get(url).success(function(data, headers, status, config) {
       $rootScope.ginfo = data;
-      $ionicLoading.hide()
+      $ionicLoading.hide();
+      $ionicHistory.clearCache();
       $state.go('profile')
     })
   }
