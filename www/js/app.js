a13da833a2d21d1007cf96039771c669ec119e2d
jack hanford <jackhanford@gmail.com>
2015-02-21 18:30:02 -0800

    fixing templates..

--- www/js/app.js
+++ www/js/app.js
@@ -20,51 +20,51 @@ angular.module('MobileGit', ['ionic', 'ngMaterial', 'GithubService', 'profile', 
     .state('search', {
       url: "/search",
       controller: 'searchCtrl',
-      templateUrl: "templates/search.html"
+      templateUrl: "./js/templates/search.html"
     })
     .state('searchpage', {
       url: "/list",
-      templateUrl: "templates/searchpage.html",
+      templateUrl: "./js/templates/searchpage.html",
       controller: 'searchviewCtrl'
     })
     .state('profile', {
       url: "/profile",
-      templateUrl: "templates/profile.html",
+      templateUrl: "./js/templates/profile.html",
       controller: 'profileCtrl'
     })
     .state('followers', {
       url: "/followers",
-      templateUrl: "templates/followers.html",
+      templateUrl: "./js/templates/followers.html",
       controller: 'followerCtrl'
     })
     .state('following', {
       url: "/following",
-      templateUrl: "templates/following.html",
+      templateUrl: "./js/templates/following.html",
       controller: 'followingCtrl'
     })
     .state('commits', {
       url: "/commits",
-      templateUrl: "templates/commits.html",
+      templateUrl: "./js/templates/commits.html",
       controller: 'commitsCtrl'
     })
     .state('project', {
       url: "/project",
-      templateUrl: "templates/project.html",
+      templateUrl: "./js/templates/project.html",
       controller: 'projectCtrl'
     })
     .state('info', {
       url: "/info",
-      templateUrl: "templates/info.html",
+      templateUrl: "./js/templates/info.html",
       controller: 'infoCtrl'
     })
     .state('codeView', {
       url: "/codeview",
-      templateUrl: "templates/code-view.html",
+      templateUrl: "./js/templates/code-view.html",
       controller: 'codeViewCtrl'
     })
     .state('intro', {
       url: "/intro",
-      templateUrl: "templates/intro.html",
+      templateUrl: "./js/templates/intro.html",
       controller: 'introCtrl'
     })
     
